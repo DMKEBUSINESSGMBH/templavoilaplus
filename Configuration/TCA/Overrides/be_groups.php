3d839c6963fb1003522f7216b7e88cd2168b913e
Jan Thelen <jan.thelen@dmk-ebusiness.com>
2023-11-30 09:29:05 +0100

    R-40098 fix be_groups

--- Configuration/TCA/Overrides/be_groups.php
+++ Configuration/TCA/Overrides/be_groups.php
@@ -1,22 +1,21 @@
 <?php
 
-defined('TYPO3_MODE') || die();
+defined('TYPO3') || die();
 
 // Adding access list to be_groups
 $tempColumns = [
     'tx_templavoilaplus_access' => [
         'label' => 'LLL:EXT:templavoilaplus/Resources/Private/Language/locallang_db.xlf:be_groups.tx_templavoilaplus_access',
         'config' => [
-            'type' => 'group',
-            'internal_type' => 'db',
-            'allowed' => 'tx_templavoilaplus_datastructure,tx_templavoilaplus_tmplobj',
-            'prepend_tname' => 1,
-            'size' => 5,
-            'autoSizeMax' => 15,
-            'multiple' => 1,
+            'type' => 'select',
+            'renderType' => 'selectMultipleSideBySide',
+            'allowNonIdValues' => 1,
+            'itemsProcFunc' => \Tvp\TemplaVoilaPlus\Service\ItemsProcFunc::class . '->mapItems',
+            'size' => 1,
             'minitems' => 0,
-            'maxitems' => 1000,
-            'show_thumbs' => 1,
+            'maxitems' => 1,
+            'showIconTable' => true,
+            'selicon_cols' => 10,
         ],
     ],
 ];
